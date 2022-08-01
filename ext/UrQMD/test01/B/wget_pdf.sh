841605a8f766c99bc418f6a168fbfd2d71b2c2b7
Gnodvi Meranov <gnodvimeranov@mail.ru>
2022-08-01 15:59:54 +0300

    add start projects

---
+++ ext/UrQMD/test01/B/wget_pdf.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+####################################################################
+
+#export http_proxy=http://127.0.0.1:3128
+export https_proxy=http://127.0.0.1:3128
+
+wget  https://drupal.star.bnl.gov/STAR/files/Bystersky_diplomka.pdf
+
+####################################################################
+
+
