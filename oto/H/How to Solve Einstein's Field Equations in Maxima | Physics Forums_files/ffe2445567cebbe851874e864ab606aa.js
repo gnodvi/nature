bb25f5db3e457386e8897b2fc169b074a15081ac
Gnodvi Meranov <gnodvimeranov@mail.ru>
2022-08-08 10:14:50 +0300

    some restructure projects

---
+++ oto/H/How to Solve Einstein's Field Equations in Maxima | Physics Forums_files/ffe2445567cebbe851874e864ab606aa.js
@@ -0,0 +1,2 @@
+function copyCopyright(){var theBody=document.getElementsByTagName("body")[0];var selection;selection=window.getSelection();var copyrightLink='<br /><br />Reference: '+document.location.href+'<br />';var copytext=selection+copyrightLink;var extraDiv=document.createElement("div");extraDiv.style.position="absolute";extraDiv.style.left="-99999px";theBody.appendChild(extraDiv);extraDiv.innerHTML=copytext;selection.selectAllChildren(extraDiv);window.setTimeout(function(){theBody.removeChild(extraDiv)},0)}
+document.body.oncopy=copyCopyright;
